e8cd250872706c50b6c50c5db51b067aa52a5021
b <b@b.b>
2024-09-21 14:46:49 +0000

    支持动态读取配置

--- gin/handlerx/Language.go
+++ gin/handlerx/Language.go
@@ -4,17 +4,17 @@ import "github.com/5-say/go-tool/logx"
 
 // 单例
 var Language = LanguageType{
-	Debug: false,
-	Force: false,
-	Use:   "",
-	Map:   make(map[string]map[string]string),
+	IsDebug: func() bool { return false },
+	Force:   false,
+	Use:     "",
+	Map:     make(map[string]map[string]string),
 }
 
 type LanguageType struct {
-	Debug bool                         // 调试模式，直接输出原始消息
-	Force bool                         // 是否强制启用语言包
-	Use   string                       // 当前使用的语言名称
-	Map   map[string]map[string]string // 语言包定义
+	IsDebug func() bool                  // 调试模式，直接输出原始消息
+	Force   bool                         // 是否强制启用语言包
+	Use     string                       // 当前使用的语言名称
+	Map     map[string]map[string]string // 语言包定义
 }
 
 func (s LanguageType) Set(languageName, key, value string) {
@@ -25,7 +25,7 @@ func (s LanguageType) Set(languageName, key, value string) {
 }
 
 func (s LanguageType) Get(key string) (value string) {
-	if s.Debug {
+	if s.IsDebug() {
 		return key
 	}
 	value, ok := s.Map[s.Use][key]
