e9e24f896895c06457a8b45693a25e7cbaadada9
b <b@b.b>
2024-11-03 10:03:15 +0000

    fixbug

--- change/ToString.go
+++ change/ToString.go
@@ -10,58 +10,72 @@ func ToString[FromType Simple | SimplePointor](data FromType) (string, error) {
 		if v == nil {
 			return "", ErrNilPointer
 		}
+		return fmt.Sprint(*v), nil
 	case *float32:
 		if v == nil {
 			return "", ErrNilPointer
 		}
+		return fmt.Sprint(*v), nil
 	case *float64:
 		if v == nil {
 			return "", ErrNilPointer
 		}
+		return fmt.Sprint(*v), nil
 	case *int:
 		if v == nil {
 			return "", ErrNilPointer
 		}
+		return fmt.Sprint(*v), nil
 	case *int8:
 		if v == nil {
 			return "", ErrNilPointer
 		}
+		return fmt.Sprint(*v), nil
 	case *int16:
 		if v == nil {
 			return "", ErrNilPointer
 		}
+		return fmt.Sprint(*v), nil
 	case *int32:
 		if v == nil {
 			return "", ErrNilPointer
 		}
+		return fmt.Sprint(*v), nil
 	case *int64:
 		if v == nil {
 			return "", ErrNilPointer
 		}
+		return fmt.Sprint(*v), nil
 	case *uint:
 		if v == nil {
 			return "", ErrNilPointer
 		}
+		return fmt.Sprint(*v), nil
 	case *uint8:
 		if v == nil {
 			return "", ErrNilPointer
 		}
+		return fmt.Sprint(*v), nil
 	case *uint16:
 		if v == nil {
 			return "", ErrNilPointer
 		}
+		return fmt.Sprint(*v), nil
 	case *uint32:
 		if v == nil {
 			return "", ErrNilPointer
 		}
+		return fmt.Sprint(*v), nil
 	case *uint64:
 		if v == nil {
 			return "", ErrNilPointer
 		}
+		return fmt.Sprint(*v), nil
 	case *bool:
 		if v == nil {
 			return "", ErrNilPointer
 		}
+		return fmt.Sprint(*v), nil
 	}
-	return fmt.Sprint(data), nil
+	return "", ErrRange
 }
