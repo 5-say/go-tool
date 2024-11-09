d2ae4d886955deaf0af7b1d1d9bd2c746bc3e4d7
b <b@b.b>
2024-11-09 09:30:50 +0000

    补充注释

--- httpx/ResponseT.go
+++ httpx/ResponseT.go
@@ -1,20 +1,8 @@
 package httpx
 
-import (
-	"encoding/json"
-)
-
 // ResponseT ..
 type ResponseT struct {
 	Error      error
 	Body       []byte
 	StatusCode int
-}
-
-// 解析响应，绑定到对象
-func (s ResponseT) BindJSON(obj any) (statusCode int, err error) {
-	if s.Error != nil {
-		return s.StatusCode, s.Error
-	}
-	return s.StatusCode, json.Unmarshal(s.Body, obj)
 }
